0f16d8c85da7d91a82784ae63beede6a0cd743ce
Karl J. Overå <k@rl.run>
2026-07-24 13:19:10 +0200

    feat: add failSpan for strings

--- lib/src/main/kotlin/no/nav/tsm/ktor/otel/Otel.kt
+++ lib/src/main/kotlin/no/nav/tsm/ktor/otel/Otel.kt
@@ -3,6 +3,22 @@ package no.nav.tsm.ktor.otel
 import io.opentelemetry.api.trace.Span
 import io.opentelemetry.api.trace.StatusCode
 
+fun failSpan(span: Span, event: String): String {
+    span.setStatus(StatusCode.ERROR)
+    span.addEvent(event)
+    return event
+}
+
+fun String.failSpan(): String {
+    val span = Span.current()
+
+    return failSpan(span, this)
+}
+
+fun String.failSpan(span: Span): String {
+    return failSpan(span, this)
+}
+
 fun failSpan(span: Span, exception: Throwable): Throwable {
     span.setStatus(StatusCode.ERROR)
     span.recordException(exception)
