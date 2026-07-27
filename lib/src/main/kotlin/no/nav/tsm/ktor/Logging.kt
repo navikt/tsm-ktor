a17e7d077fade814b9b31bd30866cd69cf4e0a48
Karl J. Overå <k@rl.run>
2026-07-27 13:13:15 +0200

    feat: add loggers that apps can use directly

--- lib/src/main/kotlin/no/nav/tsm/ktor/Logging.kt
+++ lib/src/main/kotlin/no/nav/tsm/ktor/Logging.kt
@@ -3,7 +3,11 @@ package no.nav.tsm.ktor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-internal fun logger(): Logger =
-    LoggerFactory.getLogger(
-        StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).callerClass
-    )
+fun logger(): Logger =
+    LoggerFactory.getLogger(StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).callerClass)
+
+/**
+ * This requires configuration in nais.yaml to work, as well as configuration in logback.xml!
+ */
+fun teamLogger(): Logger =
+    LoggerFactory.getLogger("teamlog.${StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).callerClass}")
