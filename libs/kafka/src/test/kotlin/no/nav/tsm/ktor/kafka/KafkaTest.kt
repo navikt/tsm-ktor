615c573537aa5fda8d1c85e6afbd3bd1aedf828e
Karl J. Overå <k@rl.run>
2026-07-31 11:47:59 +0200

    chore: test KafkaConsumer with multiple topics

--- libs/kafka/src/test/kotlin/no/nav/tsm/ktor/kafka/KafkaTest.kt
+++ libs/kafka/src/test/kotlin/no/nav/tsm/ktor/kafka/KafkaTest.kt
@@ -1,6 +1,7 @@
 package no.nav.tsm.ktor.kafka
 
 import com.typesafe.config.ConfigFactory
+import io.kotest.matchers.equals.shouldEqual
 import io.ktor.server.config.HoconApplicationConfig
 import io.ktor.server.testing.testApplication
 import io.mockk.mockk
@@ -32,7 +33,7 @@ class KafkaTest {
         val kafka =
             ConfluentKafkaContainer("confluentinc/cp-kafka:8.1.0").apply {
                 start()
-                createTopics(listOf("example-topic"))
+                createTopics(listOf("example-topic", "other-topic"))
             }
 
         val producer = createProducer(kafka.bootstrapServers)
@@ -98,8 +99,78 @@ class KafkaTest {
             tombstoneMock("test-key")
         }
 
-        val offset = admin.getOffset("test-group-id")
+        val offset = admin.getOffset("example-topic", "test-group-id")
         assert(offset == 3L) { "Expected offset to be 3, but was $offset" }
+    }
+
+    @Test
+    fun `should support multiple topics`() = testApplication {
+        environment {
+            config = HoconApplicationConfig(ConfigFactory.parseString(hocon))
+        }
+
+        val tombOneMock = mockk<(String) -> Unit>(relaxed = true)
+        val recordOneMock = mockk<(MyRecord) -> Unit>(relaxed = true)
+        val tombTwoMock = mockk<(String) -> Unit>(relaxed = true)
+        val recordTwoMock = mockk<(MyRecord) -> Unit>(relaxed = true)
+
+        install(KafkaConsumer) {
+            groupId = "test-group-id"
+            pollDuration = 1.seconds
+            retryDuration = 1.seconds
+
+            topic<MyRecord>(
+                name = "example-topic",
+                onTombstone = { key ->
+                    tombOneMock(key)
+                },
+                onRecord = { record ->
+                    recordOneMock(record)
+                },
+            )
+
+            topic<MyRecord>(
+                name = "other-topic",
+                onTombstone = { key ->
+                    tombTwoMock(key)
+                },
+                onRecord = { record ->
+                    recordTwoMock(record)
+                },
+            )
+        }
+
+        startApplication()
+
+        producer.send(
+            topic = "example-topic",
+            key = "test-key",
+            value = """{"sykmeldingId":"124","someOthervalue":"abc","hasManyValues":true}""".toByteArray(),
+        )
+        producer.send(
+            topic = "example-topic",
+            key = "test-key",
+            value = null,
+        )
+        producer.send(
+            topic = "other-topic",
+            key = "test-key",
+            value = """{"sykmeldingId":"124","someOthervalue":"abc","hasManyValues":true}""".toByteArray(),
+        )
+        producer.send(
+            topic = "other-topic",
+            key = "test-key",
+            value = null,
+        )
+
+        verify(timeout = 5000) { recordOneMock(MyRecord("124", "abc", true)) }
+        verify(timeout = 5000) { recordTwoMock(MyRecord("124", "abc", true)) }
+
+        val exampleOffset = admin.getOffset("example-topic", "test-group-id")
+        exampleOffset shouldEqual 2L
+
+        val otherOffset = admin.getOffset("other-topic", "test-group-id")
+        otherOffset shouldEqual 2L
     }
 
     @Test
@@ -182,8 +253,9 @@ private fun createAdmin(bootstrapServers: String): AdminClient {
     return AdminClient.create(props)
 }
 
-private fun AdminClient.getOffset(groupId: String): Long {
+private fun AdminClient.getOffset(topic: String, groupId: String): Long {
     val offsets = this.listConsumerGroupOffsets(groupId).partitionsToOffsetAndMetadata().get()
-    val partition = offsets.keys.first()
-    return offsets[partition]?.offset() ?: 0
+    val partition =
+        offsets.keys.find { it.topic() == topic } ?: throw IllegalStateException("Found no topic \"$topic\"")
+    return offsets[partition]?.offset() ?: throw IllegalStateException("Found no offset for topic \"$topic\"")
 }
